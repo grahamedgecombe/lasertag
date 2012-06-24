b3bfd73cd1a36f93e6377815ab5854b4b29e3314
Graham Edgecombe <graham@grahamedgecombe.com>
2012-06-24 23:15:06 +0100

    Added documentation to functions in speaker.h.

--- src/lasertag/speaker.h
+++ src/lasertag/speaker.h
@@ -1,8 +1,13 @@
 #ifndef LASERTAG_SPEAKER_H
 #define LASERTAG_SPEAKER_H
 
+/* Initializes the speaker. */
 void speaker_init(void);
+
+/* Turns on the speaker and makes it play a tone of the given frequency. */
 void speaker_tone(int hz);
+
+/* Turns off the speaker. */
 void speaker_off(void);
 
 #endif
