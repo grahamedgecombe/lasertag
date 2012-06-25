715e995b1bd9be23045aca442dbcf1e5fe395e21
Graham Edgecombe <graham@grahamedgecombe.com>
2012-06-25 09:59:13 +0100

    Added clock code for keeping track of the number of microseconds.

--- src/lasertag/clock.c
+++ src/lasertag/clock.c
@@ -1,5 +1,17 @@
 #include <lasertag/clock.h>
+#include <avr/interrupt.h>
 #include <avr/io.h>
+#include <util/atomic.h>
+
+/* The number of microseconds between overflow interrupts. */
+#define CLOCK_USECS_PER_OVERFLOW (256 * CLOCK_USECS_PER_TICK)
+
+static volatile uint32_t clock_overflows = 0;
+
+ISR(TIMER2_OVF_vect)
+{
+  clock_overflows++;
+}
 
 void clock_init(void)
 {
@@ -12,5 +24,30 @@ void clock_init(void)
    */
   TCCR2A = 0;
   TCCR2B = (1 << CS22) | (1 << CS21);
+
+  /* Enable the overflow interrupt. */
+  TIMSK2 = (1 << TOIE2);
+}
+
+uint32_t clock_micros(void)
+{
+  uint8_t ticks;
+  uint32_t overflows;
+
+  ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
+  {
+    ticks = TCNT2;
+    overflows = clock_overflows;
+  }
+
+  return (uint32_t) ticks * CLOCK_USECS_PER_TICK + overflows * CLOCK_USECS_PER_OVERFLOW;
+}
+
+uint32_t clock_delta(uint32_t now, uint32_t prev)
+{
+  if (now >= prev)
+    return now - prev;
+  else
+    return UINT32_MAX - prev + now;
 }
 
--- src/lasertag/clock.h
+++ src/lasertag/clock.h
@@ -1,6 +1,8 @@
 #ifndef LASERTAG_CLOCK_H
 #define LASERTAG_CLOCK_H
 
+#include <stdint.h>
+
 /* The prescaler used by the timer. */
 #define CLOCK_PRESCALER 256
 
@@ -9,6 +11,15 @@
 
 /* Initializes the general-purpose 'clock' timer. */
 void clock_init(void);
+
+/* Returns the number of microseconds since the clock started. */
+uint32_t clock_micros(void);
+
+/*
+ * Returns the number of microseconds between two times, taking overflow into
+ * account.
+ */
+uint32_t clock_delta(uint32_t now, uint32_t prev);
 
 #endif
 
